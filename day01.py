1bd1216f0bca30b1f9a8678e80303c69b51166c9
scottdharris <scottdharris11@yahoo.com>
2024-12-11 19:12:11 -0500

    day 1, part 2

--- day01.py
+++ day01.py
@@ -20,15 +20,36 @@ def solve_part1(instructions: list[str]):
     return abs(pos[0]) + abs(pos[1])
 
 @runner("Day 1", "Part 2")
-def solve_part2(lines: list[str]):
+def solve_part2(instructions: list[str]):
     """part 2 solving function"""
-    return 0
+    pos = [0,0]
+    cur = 0
+    locs = set()
+    for i in instructions:
+        if i[0] == 'R':
+            cur = (cur + 1) % 4
+        else:
+            cur = (cur - 1) % 4
+        steps = int(i[1:])
+        hq = False
+        for _ in range(steps):
+            pos[0] += directions[cur][0]
+            pos[1] += directions[cur][1]
+            loc = (pos[0], pos[1])
+            if loc in locs:
+                hq = True
+                break
+            locs.add(loc)
+        if hq:
+            break
+    return abs(pos[0]) + abs(pos[1])
 
 # Data
 data = read_lines("input/day01/input.txt")[0].split(", ")
 sample = """R2, L3""".split(", ")
 sample2 = """R2, R2, R2""".split(", ")
 sample3 = """R5, L5, R5, R3""".split(", ")
+sample4 = """R8, R4, R4, R8""".split(", ")
 
 # Part 1
 assert solve_part1(sample) == 5
@@ -37,5 +58,5 @@ assert solve_part1(sample3) == 12
 assert solve_part1(data) == 242
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample4) == 4
+assert solve_part2(data) == 150
