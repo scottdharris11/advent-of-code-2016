3741ceb456d62f2091577b36d6213bdb1fd258a0
scottdharris <scottdharris11@yahoo.com>
2024-12-11 19:03:26 -0500

    day 1, part 1

--- day01.py
+++ day01.py
@@ -2,10 +2,22 @@
 from utilities.data import read_lines
 from utilities.runner import runner
 
+directions = [(0,1),(1,0),(0,-1),(-1,0)]
+
 @runner("Day 1", "Part 1")
-def solve_part1(lines: list[str]):
+def solve_part1(instructions: list[str]):
     """part 1 solving function"""
-    return 0
+    pos = [0,0]
+    cur = 0
+    for i in instructions:
+        if i[0] == 'R':
+            cur = (cur + 1) % 4
+        else:
+            cur = (cur - 1) % 4
+        steps = int(i[1:])
+        pos[0] += directions[cur][0] * steps
+        pos[1] += directions[cur][1] * steps
+    return abs(pos[0]) + abs(pos[1])
 
 @runner("Day 1", "Part 2")
 def solve_part2(lines: list[str]):
@@ -13,12 +25,16 @@ def solve_part2(lines: list[str]):
     return 0
 
 # Data
-data = read_lines("input/day01/input.txt")
-sample = """""".splitlines()
+data = read_lines("input/day01/input.txt")[0].split(", ")
+sample = """R2, L3""".split(", ")
+sample2 = """R2, R2, R2""".split(", ")
+sample3 = """R5, L5, R5, R3""".split(", ")
 
 # Part 1
-assert solve_part1(sample) == 0
-assert solve_part1(data) == 0
+assert solve_part1(sample) == 5
+assert solve_part1(sample2) == 2
+assert solve_part1(sample3) == 12
+assert solve_part1(data) == 242
 
 # Part 2
 assert solve_part2(sample) == 0
