b68fcbe99f2efbf202ab6e5be02f2c6547d11ed1
scottdharris <scottdharris11@yahoo.com>
2024-12-15 21:18:10 -0500

    day 4, part 2

--- day04.py
+++ day04.py
@@ -6,29 +6,51 @@ from utilities.runner import runner
 def solve_part1(lines: list[str]):
     """part 1 solving function"""
     sectorsum = 0
-    for l in lines:
-        namei = l.rindex("-")
-        csi = l.index("[")
-        name = l[:namei]
-        sector = int(l[namei+1:csi])
-        cs = l[csi+1:l.index("]")]
-        letter_counts = {}
-        for c in name:
-            if c == "-":
-                continue
-            letter_counts[c] = letter_counts.get(c, 0) + 1
-        values = sorted(letter_counts.items(), key=lambda x: (-x[1], x[0]))
-        comp = ""
-        for i in range(5):
-            comp += str(values[i][0])
-        if comp == cs:
+    for line in lines:
+        name, sector, cs = parse(line)
+        if valid(name, cs):
             sectorsum += sector
     return sectorsum
 
 @runner("Day 4", "Part 2")
-def solve_part2(lines: list[str]):
+def solve_part2(lines: list[str], match: str):
     """part 2 solving function"""
+    for line in lines:
+        name, sector, cs = parse(line)
+        if not valid(name, cs):
+            continue
+        dname = ""
+        offset = sector % 26
+        for c in name:
+            if c == "-":
+                dname += " "
+            else:
+                o = ord(c) + offset
+                if o > ord('z'):
+                    o = ord('a') + o - ord('z') - 1
+                dname += chr(o)
+        if dname == match:
+            return sector
     return 0
+
+def parse(s: str) -> tuple[str,int,str]:
+    """parse values from string"""
+    namei = s.rindex("-")
+    csi = s.index("[")
+    return (s[:namei], int(s[namei+1:csi]), s[csi+1:s.index("]")])
+
+def valid(name: str, cs: str) -> bool:
+    """check if data is valid"""
+    letter_counts = {}
+    for c in name:
+        if c == "-":
+            continue
+        letter_counts[c] = letter_counts.get(c, 0) + 1
+    values = sorted(letter_counts.items(), key=lambda x: (-x[1], x[0]))
+    comp = ""
+    for i in range(5):
+        comp += str(values[i][0])
+    return comp == cs
 
 # Data
 data = read_lines("input/day04/input.txt")
@@ -39,8 +61,8 @@ totally-real-room-200[decoy]""".splitlines()
 
 # Part 1
 assert solve_part1(sample) == 1514
-assert solve_part1(data) == 0
+assert solve_part1(data) == 185371
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(["qzmt-zixmtkozy-ivhz-343[zimth]"], "very encrypted name") == 343
+assert solve_part2(data, "northpole object storage") == 984
