4cea6b7d366d28604c0a9219deb0ff161f601037
scottdharris <scottdharris11@yahoo.com>
2024-12-29 09:05:25 -0500

    day 7 part 2

--- day07.py
+++ day07.py
@@ -23,7 +23,24 @@ def solve_part1(lines: list[str]) -> int:
 @runner("Day 7", "Part 2")
 def solve_part2(lines: list[str]) -> int:
     """part 2 solving function"""
-    return 0
+    r = re.compile(r'(\[[a-z]*?\])')
+    ssl = 0
+    for line in lines:
+        hypernets = r.findall(line)
+        for s in r.sub('|', line).split('|'):
+            for a in aba(s):
+                b = bab(a)
+                for h in hypernets:
+                    if b in h:
+                        ssl += 1
+                        break
+                else:
+                    continue
+                break
+            else:
+                continue
+            break
+    return ssl
 
 def abba(s: str) -> bool:
     """determine if abba exists within string"""
@@ -34,17 +51,35 @@ def abba(s: str) -> bool:
             return True
     return False
 
+def aba(s: str) -> list[str]:
+    """find all area-broadcast accessors within the string"""
+    if len(s) < 3:
+        return False
+    found = []
+    for i in range(len(s)-2):
+        if s[i] != s[i+1] and s[i] == s[i+2]:
+            found.append(s[i:i+3])
+    return found
+
+def bab(s: str) -> str:
+    """convert an area-broadcast accessor to a byte allocation block"""
+    return s[1] + s[0] + s[1]
+
 # Data
 data = read_lines("input/day07/input.txt")
 sample = """abba[mnop]qrst
 abcd[bddb]xyyx
 aaaa[qwer]tyui
 ioxxoj[asdfgh]zxcvbn""".splitlines()
+sample2 = """aba[bab]xyz
+xyx[xyx]xyx
+aaa[kek]eke
+zazbz[bzb]cdb""".splitlines()
 
 # Part 1
 assert solve_part1(sample) == 2
 assert solve_part1(data) == 105
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample2) == 3
+assert solve_part2(data) == 258
