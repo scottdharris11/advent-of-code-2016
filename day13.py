6975abaacc416a7f06d9dc943174d42cb0075113
scottdharris <scottdharris11@yahoo.com>
2025-01-05 19:35:54 -0500

    day 13 setup

---
+++ day13.py
@@ -0,0 +1,20 @@
+"""utility imports"""
+from utilities.runner import runner
+
+@runner("Day 13", "Part 1")
+def solve_part1(designer: int) -> int:
+    """part 1 solving function"""
+    return 0
+
+@runner("Day 13", "Part 2")
+def solve_part2(designer: int) -> int:
+    """part 2 solving function"""
+    return 0
+
+# Part 1
+assert solve_part1(10) == 11
+assert solve_part1(1362) == 0
+
+# Part 2
+assert solve_part2(10) == 0
+assert solve_part2(1362) == 0
