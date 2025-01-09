c320e67cd99d21e6d4217b86cb36fbe64fcb368f
scottdharris <scottdharris11@yahoo.com>
2025-01-09 13:21:48 -0500

    day 19 setup

---
+++ day19.py
@@ -0,0 +1,20 @@
+"""utility imports"""
+from utilities.runner import runner
+
+@runner("Day 19", "Part 1")
+def solve_part1(count: int) -> int:
+    """part 1 solving function"""
+    return 0
+
+@runner("Day 19", "Part 2")
+def solve_part2(count: int) -> int:
+    """part 2 solving function"""
+    return 0
+
+# Part 1
+assert solve_part1(5) == 3
+assert solve_part1(3017957) == 0
+
+# Part 2
+assert solve_part2(5) == 0
+assert solve_part2(3017957) == 0
