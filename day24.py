5d3aad658d3311327440acc972ae409cbb2f8dd1
scottdharris <scottdharris11@yahoo.com>
2025-01-15 13:55:23 -0500

    day 24, part 2

--- day24.py
+++ day24.py
@@ -6,31 +6,14 @@ from utilities.search import Search, Searcher, SearchMove
 @runner("Day 24", "Part 1")
 def solve_part1(lines: list[str]) -> int:
     """part 1 solving function"""
-    ds = DuctSearcher(lines)
-    s = Search(ds)
-    
-    # compute best routes between all locations
-    locations = list(ds.locations.keys())
-    locations.sort()
-    best_routes = {}
-    for i, f in enumerate(locations):
-        for t in locations[i+1:]:
-            start = (ds.locations[t], ds.locations[f])
-            solution = s.best(SearchMove(0, start))
-            if solution is None:
-                return -1
-            best_routes[f+t] = solution.cost
-            best_routes[t+f] = solution.cost
-    
-    # look at each potential location paths for best route
-    paths = []
-    potential_paths(locations[1:], "", paths)
+    locations, routes = best_routes(lines)
+    paths = potential_paths(locations[1:], "", [])
     best = -1
     for path in paths:
         f = '0'
         steps = 0
         for t in path:
-            steps += best_routes[f+t]
+            steps += routes[f+t]
             f = t
         if best == -1 or steps < best:
             best = steps
@@ -39,7 +22,19 @@ def solve_part1(lines: list[str]) -> int:
 @runner("Day 24", "Part 2")
 def solve_part2(lines: list[str]) -> int:
     """part 2 solving function"""
-    return 0
+    locations, routes = best_routes(lines)
+    paths = potential_paths(locations[1:], "", [])
+    best = -1
+    for path in paths:
+        f = '0'
+        steps = 0
+        for t in path:
+            steps += routes[f+t]
+            f = t
+        steps += routes[f+'0']
+        if best == -1 or steps < best:
+            best = steps
+    return best
 
 class DuctSearcher(Searcher):
     """search implementation for the storage migrations"""
@@ -74,7 +69,24 @@ class DuctSearcher(Searcher):
         """calculate distance from the goal"""
         return abs(obj[0][0] - obj[1][0]) + abs(obj[0][1] - obj[1][1])
 
-def potential_paths(locations: list[str], path: str, paths: list[str]) -> None:
+def best_routes(lines: list[str]) -> tuple[list[str],dict[str,int]]:
+    """compute best routes between all locations"""
+    ds = DuctSearcher(lines)
+    s = Search(ds)
+    locations = list(ds.locations.keys())
+    locations.sort()
+    br = {}
+    for i, f in enumerate(locations):
+        for t in locations[i+1:]:
+            start = (ds.locations[t], ds.locations[f])
+            solution = s.best(SearchMove(0, start))
+            if solution is None:
+                return -1
+            br[f+t] = solution.cost
+            br[t+f] = solution.cost
+    return locations, br
+
+def potential_paths(locations: list[str], path: str, paths: list[str]) -> list[str]:
     """build set of potential paths based on the set of locations"""
     if len(locations) == 0:
         paths.append(path)
@@ -82,6 +94,7 @@ def potential_paths(locations: list[str], path: str, paths: list[str]) -> None:
         nlocs = list(locations)
         nlocs.remove(l)
         potential_paths(nlocs, path + l, paths)
+    return paths
 
 # Data
 data = read_lines("input/day24/input.txt")
@@ -96,5 +109,5 @@ assert solve_part1(sample) == 14
 assert solve_part1(data) == 412
 
 # Part 2
-assert solve_part2(sample) == 0
-assert solve_part2(data) == 0
+assert solve_part2(sample) == 20
+assert solve_part2(data) == 664
